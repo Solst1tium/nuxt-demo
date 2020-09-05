4bc014c71aecc17cb3054b485a7261f15e2ee9bb
Solst1tium <solange.leiva@mayor.cl>
2020-09-04 21:24:25 -0400

    se creo la pages rickyAndMorty

---
+++ pages/rickyAndMorty.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="12" sm="10" offset-sm="1">
+        <v-container fluid>
+          <v-row>
+            <v-col v-for="personaje in personajes" :key="personaje.id" class="d-flex child-flex" cols="4">
+              <v-card tile class="d-flex flex-column">
+                <v-img :src="personaje.imagen" aspect-ratio="1" class="grey lighten-2" />
+                <v-card-title> {{ personaje.nombre }}</v-card-title>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+<script>
+export default {
+  name: 'RickyAndMorty',
+
+  data () {
+    return {
+      personajes: [
+        {
+          id: 1,
+          nombre: 'Morty',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+        },
+        {
+          id: 2,
+          nombre: 'Rick',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+        },
+        {
+          id: 3,
+          nombre: 'Summer',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+        },
+        {
+          id: 4,
+          nombre: 'Beth',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
+        },
+        {
+          id: 5,
+          nombre: 'Jerry',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg'
+        }
+      ]
+    }
+  }
+}
+
+</script>
